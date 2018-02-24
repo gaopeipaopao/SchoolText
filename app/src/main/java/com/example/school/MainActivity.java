1dbe114bb01a973c5361f0afc76e6101668d548c
gaopei <gaopeigpgp@126.com>
2018-02-24 22:13:40 +0800

    密码的显隐和记住密码

--- app/src/main/java/com/example/school/MainActivity.java
+++ app/src/main/java/com/example/school/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.school;
 
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.RippleDrawable;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private EditText textNames;
     private EditText  passWords;
+    private CheckBox  eye_enter;
+    private CheckBox  rember_Password;
     private String textName="";
     private String passWord="";
     private String code = "";
@@ -36,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText id_code;
     private TextView textView;
     private String cookieStr;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
     OkHttpClient okHttpClient = new OkHttpClient().newBuilder().cookieJar(new LocalCookieJar()).build();
 
     @Override
@@ -46,13 +57,62 @@ public class MainActivity extends AppCompatActivity {
         if(actionBar!=null){
             actionBar.hide();
         }
+        sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+        eye_enter=(CheckBox)findViewById(R.id.eye_enter);
+        rember_Password=(CheckBox)findViewById(R.id.rember_passWord);
         textNames=(EditText)findViewById(R.id.textName);
         passWords=(EditText)findViewById(R.id.passWord);
         textView = (TextView) findViewById(R.id.text);
         enter = (Button) findViewById(R.id.enter_a);
         id_code = (EditText) findViewById(R.id.id_code);
         imageView = (ImageView) findViewById(R.id.imageCode);
+        final Boolean rember=sharedPreferences.getBoolean("记住密码",false);
         Log.d("aaaaaaa", "aaaaaaaa");
+        if(rember){
+            String name=sharedPreferences.getString("学号","");
+            String password=sharedPreferences.getString("密码","");
+            rember_Password.setChecked(true);
+            textNames.setText(name);
+            passWords.setText(password);
+        }
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rember_Password.isChecked()){
+                    editor=sharedPreferences.edit();
+                    editor.putString("学号",textNames.getText().toString());
+                    editor.putString("密码",passWords.getText().toString());
+                    editor.putBoolean("记住密码",true);
+                }
+                if(!rember_Password.isChecked()){
+                    editor=sharedPreferences.edit();
+                    editor.putString("学号","");
+                    editor.putString("密码","");
+                    editor.putBoolean("记住密码",false);
+                }
+                editor.apply();
+                textName=textNames.getText().toString();
+                Log.d("Nmae",textName);
+                passWord=passWords.getText().toString();
+                Log.d("word",passWord);
+                code = id_code.getText().toString();
+                Log.d("code", code);
+                send();
+                // send2();
+            }
+        });
+        eye_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(eye_enter.isChecked()){
+                    passWords.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    eye_enter.setButtonDrawable(R.drawable.eye_open);
+                }else {
+                    passWords.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    eye_enter.setButtonDrawable(R.drawable.eye_close);
+                }
+            }
+        });
         SetOkHttp.setHttpGet(okHttpClient, "http://222.24.62.120/CheckCode.aspx", new Callback() {
             @Override
             public void onFailure(okhttp3.Call call, IOException e) {
@@ -87,19 +147,6 @@ public class MainActivity extends AppCompatActivity {
                         imageView.setImageBitmap(bitmap);//ImageView 显示验证码的组件
                     }
                 });
-            }
-        });
-        enter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textName=textNames.getText().toString();
-                Log.d("Nmae",textName);
-                passWord=passWords.getText().toString();
-                Log.d("word",passWord);
-                code = id_code.getText().toString();
-                Log.d("code", code);
-                send();
-                // send2();
             }
         });
     }
